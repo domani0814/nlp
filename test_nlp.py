ea0f5516384aff8a1c65a84425b83cce38eaa5de
domani0814 <domani_0814@icloud.com>
2023-10-02 16:46:26 +0900

    test1

--- test_nlp.py
+++ test_nlp.py
@@ -1,2 +1,9 @@
-print("hello")
-print(4+1)
+import pandas as pd
+
+file_path = 'Desktop/workarea/summery/webtoon.csv'
+
+df = pd.read_csv(file_path)
+df = pd.DataFrame(df)
+df = df[['title','outline']]
+df = df.drop_duplicates(subset=['title'])
+print(df)
