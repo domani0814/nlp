b6c64ffc1ee8d2f8a328f48532e107920aaa7858
domani0814 <domani_0814@icloud.com>
2023-10-05 01:13:50 +0900

    fuck

--- test_nlp.py
+++ test_nlp.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
-file_path = 'Desktop/workarea/summery/webtoon.csv'
+file_path = '/Users/choedohyeon/desktop/workarea/summery/webtoon.csv'
 
 df = pd.read_csv(file_path)
 df = pd.DataFrame(df)
-df = df[['title','outline']]
+df = df[['title', 'outline']]
 df = df.drop_duplicates(subset=['title'])
-print(df)
+df = df.dropna()
+df = df.reset_index(drop=True)
+
+print(df.head())
